01684b5487c888b7aa78698b719f0d9b1c27afd5
xbrnovik <xbrnovik@node.mendelu.cz>
2019-02-03 19:18:16 +0100

    Repaired lat-long opposite notation.

--- Talula/Talula/Storage/Meteorite+CoreDataProperties.swift
+++ Talula/Talula/Storage/Meteorite+CoreDataProperties.swift
@@ -28,18 +28,20 @@ extension Meteorite {
         // Checks meteorite attributes - fallen meteorite has its ID and location.
         guard
             let id = jsonDictionary["id"] as? String,
-            let latitudeString = jsonDictionary["reclat"] as? String,
+            // NASA stores probably opposite notation, "reclong" is latitude with values from -180 to 180.
+            let latitudeString = jsonDictionary["reclong"] as? String,
             let latitude = Double(latitudeString),
-            let longitudeString = jsonDictionary["reclong"] as? String,
+            // NASA stores probably opposite notation, "reclat" is longitude with values from -90 to 90.
+            let longitudeString = jsonDictionary["reclat"] as? String,
             let longitude = Double(longitudeString)
         else {
             throw NSError(domain: "", code: 100, userInfo: nil)
         }
-        //Sets obligatory attributes.
+        // Sets obligatory attributes.
         self.meteoriteId = id
         self.longitude = latitude
         self.latitude = longitude
-        //Sets voluntary attributes.
+        // Sets voluntary attributes.
         self.name = jsonDictionary["name"] as? String
         self.geotype = jsonDictionary["recclass"] as? String
         if let mass = jsonDictionary["mass"] as? NSString {
